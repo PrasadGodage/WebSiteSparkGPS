4e2aa53a70dccb6cf42abd09c1e92bbf1a9dc71e
dipalirahane <137761526+dipalirahane@users.noreply.github.com>
2023-07-05 18:28:09 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+var typed=new typed(".auto-type", {
+
+        strings: ["aaaa","ccccc","ffff"],
+        typespeed:150,
+        backspeed:150,
+        loop:true
+      });
